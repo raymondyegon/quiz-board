3c6dd26d32577bd457d14cd0976b322181931720
Raymond Yegon <raymondyegon17@gmail.com>
2019-02-25 07:47:29 +0300

    All selected answers putted to array

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
 $(document).ready(function() {
-  var answers ;
+  var answer1 ;
+  var answer2 ;
+  var answer3 ;
+  var answer4 ;
+  var answer5 ;
+  var answers = [] ;
   $("#submit").on('click',function() {
-    answers = $("input[name='quiz1']:checked").val();
-      alert (answers)
+    answer1 = $("input[name='quiz1']:checked").val();
+    answer2 = $("input[name='quiz2']:checked").val();
+    answer3 = $("input[name='quiz3']:checked").val();
+    answer4 = $("input[name='quiz4']:checked").val();
+    answer5 = $("input[name='quiz5']:checked").val();
+    answers = [answer1, answer2, answer3, answer4, answer5]
+  if () {
+
+  } else {
+
+  }
  });
 });
